d31bc3cb679d59fa7c29535f81fb851541d4cde4
Petr Janoš <koksa.janos@gmail.com>
2019-12-20 20:36:57 +0100

    bounding_box
    fix

---
+++ poznamky.py
@@ -0,0 +1,12 @@
+# find max and min:
+
+test_list = [[3, 7, 6], [1, 3, 5], [9, 3, 2]]
+
+## option 1: pro každý index co seznam má
+res = [min(idx) for idx in zip(*test_list)]
+print("The Minimum of each index list is : " + str(res))
+
+## option 2: pro index, který chceme
+a_min = min(i[0] for i in test_list)
+[i[0] for i in test_list if i[1] == a_min and i[0] > 10000] + [a_min]
+print(a_min)
--- split.py
+++ split.py
@@ -5,12 +5,28 @@ with open ("points.geojson", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 
-def sort_points():
-    a = []
+def bounding_box():
+    souradnice_list = []
+    # vypiš z "features" všechny "coordinates" z kat. geometry
     for feat in data['features']:
-        a.append(feat['geometry']['coordinates'])
-    print("Původní seznam je: \n", a)
-    a.sort(key=lambda x: x[0])
-    print("Seřazený seznam je: \n",a)
-sort_points()
+        # přidej do seznamu
+        souradnice_list.append(feat['geometry']['coordinates'])
+    print("Původní seznam je: \n", souradnice_list)
 
+    # seřaď seznam
+    souradnice_list.sort(key=lambda x: x[0])
+    print("Seřazený seznam je: \n", souradnice_list)
+
+    # najdi maxima a minima
+    vlevo = min(i[0] for i in souradnice_list)
+    vpravo = max(i[0] for i in souradnice_list)
+    dole = min(i[1] for i in souradnice_list)
+    hore = max(i[1] for i in souradnice_list)
+    print("\n BoundingBox: \n", "vlevo: ", vlevo, "\n", "vpravo: ", vpravo,"\n", "dole: ", dole, "\n", "hore: ", hore)
+
+    # rozděl na poloviny
+    mid_x = (vlevo + vpravo) / 2
+    mid_y = (dole + hore) / 2
+    print("\n mid_x", mid_x, "\n mid_y", mid_y)
+
+bounding_box()
